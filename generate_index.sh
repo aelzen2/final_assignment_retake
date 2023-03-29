e686e3dbca3f15a5ef13cc1279f33c5989373a39
aelzen2 <anouk.vandenelzen2@ru.nl>
2023-03-29 20:54:45 +0200

    final version of retake

--- generate_index.sh
+++ generate_index.sh
@@ -2,14 +2,14 @@ input_file=/scratch/bash_course/lecture_4/retake/genomes.txt
 
 1> SAindex_log.txt
 
-output_directory=final_assignment_anouk 
+output_dir=final_assignment_anouk 
 
 source $(dirname $(dirname $(which mamba)))/etc/profile.d/conda.sh 
 conda activate alignment2
 
 for line in $(cat $input_file); do
 nice genomepy install $line
-nice STAR --runMode genomeGenerate --genomeFastaFiles .local/share/genomes/$line/$line.fa --genomeDir $output_directory/$line;
+nice STAR --runMode genomeGenerate --genomeFastaFiles .local/share/genomes/$line/$line.fa --genomeDir $output_dir/$line;
 done >> SAindex_log.txt
 
 echo  Anouk van den Elzen 
